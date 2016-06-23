5e50691bd230b9c9acb0946d8a5dc817fdc02bb5
Haley Akchurin <hdakchurin@gmail.com>
2016-06-23 10:28:51 -0500

    challenges

---
+++ challenge.js
@@ -0,0 +1,66 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: identifying or naming the variable we are about to set it to
+
+
+givenName = "Jess";
+// Q: What is `givenName` set to right now?
+// A: Jess
+
+
+givenName = givenName;
+// Q: What is `givenName` set to right now?
+// A:givenName
+
+
+var greeting = "Hello, how are you? " + givenName;
+// Q: What is `greeting` set to?
+// A: Hello, how are you and the givenName which is set to Jess
+
+
+/*=========================================================
+     Part 2: Simple Math
+*/
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: the variable we names high (50) - the variable we named low (10)
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: the variable we named high (50) - 5
+
+
+/* =========================================================
+     Part 3: Expressions - You Write Code!
+*/
+
+// Create a variable to calculate Jess' age
+// The answer should read "Jess is 33 years old"
+// The answer should not be written in a comment.
+var born = 1983;
+var today = 2016;
+
+// Answers Below:
+
+var age = today - born;
+
+// Store some information following in variables.
+var yourName;
+var instructorName;
+
+var Haley;
+var jess;
+// Update the variables above so the expression reads correctly.
+// Answers Below:
+
+
+// Final Statement
+var statement = "My name is " yourName + ", and I am taking a class at The Iron Yard. My instructor's name is " + instructorName;
